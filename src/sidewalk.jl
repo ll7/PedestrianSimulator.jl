520a816dbacdc63804b4def3505e696c91bd4f2e
Lennart Luttkus <lennart.luttkus@uni-a.de>
2022-04-21 09:05:30 +0200

    add local dev package

--- src/sidewalk.jl
+++ src/sidewalk.jl
@@ -39,12 +39,12 @@ end;
 
 # %%
 # Geometry parameters of the sidewalk
-roadway_length = 100.
-crosswalk_length = 15.
+roadway_length = 100.0
+crosswalk_length = 15.0
 crosswalk_width = 6.0
-crosswalk_pos = roadway_length/2
+crosswalk_pos = roadway_length / 2
 sidewalk_width = 3.0
-sidewalk_pos = crosswalk_length/2 - sidewalk_width / 2
+sidewalk_pos = crosswalk_length / 2 - sidewalk_width / 2
 
 # %% [markdown]
 # Now we create the sidewalk environment.
@@ -68,19 +68,31 @@ roadway = gen_straight_roadway(2, roadway_length)
 
 # %%
 n_samples = 2 # for curve generation
-crosswalk = Lane(LaneTag(2,1), gen_straight_curve(VecE2(crosswalk_pos, -crosswalk_length/2),
-                                                  VecE2(crosswalk_pos, crosswalk_length/2),
-                                                   n_samples), width = crosswalk_width)
+crosswalk = Lane(
+    LaneTag(2, 1),
+    gen_straight_curve(
+        VecE2(crosswalk_pos, -crosswalk_length / 2),
+        VecE2(crosswalk_pos, crosswalk_length / 2),
+        n_samples
+    ),
+    width=crosswalk_width
+)
 cw_segment = RoadSegment(2, [crosswalk])
 push!(roadway.segments, cw_segment) # Append the crosswalk to the roadway
 
 # Generate the sidewalk
-top_sidewalk = Lane(LaneTag(3, TOP), gen_straight_curve(VecE2(0., sidewalk_pos),
-                                                      VecE2(roadway_length, sidewalk_pos),
-                                                        n_samples), width = sidewalk_width)
-bottom_sidewalk = Lane(LaneTag(3, BOTTOM), gen_straight_curve(VecE2(0., -(sidewalk_pos - sidewalk_width)),
-                                                          VecE2(roadway_length, -(sidewalk_pos - sidewalk_width)),
-                                                            n_samples), width = sidewalk_width)
+top_sidewalk = Lane(LaneTag(3, TOP), gen_straight_curve(VecE2(0.0, sidewalk_pos),
+        VecE2(roadway_length, sidewalk_pos),
+        n_samples), width=sidewalk_width)
+bottom_sidewalk = Lane(
+    LaneTag(3, BOTTOM),
+    gen_straight_curve(
+        VecE2(0.0, -(sidewalk_pos - sidewalk_width)),
+        VecE2(roadway_length, -(sidewalk_pos - sidewalk_width)),
+        n_samples
+    ),
+    width=sidewalk_width
+)
 
 # %% [markdown]
 # Note: we subtract the sidewalk_width from the sidewalk position so that the edge is flush with the road.
@@ -99,17 +111,24 @@ env = SidewalkEnv(roadway, crosswalk, [top_sidewalk, bottom_sidewalk]);
 # Since there is no defined `add_renderable!` method for the crosswalk and the sidewalk, we must define it ourselves.
 
 # %%
-function AutomotiveVisualization.add_renderable!(rendermodel::RenderModel, env::SidewalkEnv)
+function AutomotiveVisualization.add_renderable!(
+    rendermodel::RenderModel,
+    env::SidewalkEnv
+)
     # Render sidewalk
     for sw in env.sidewalk
         curve = sw.curve
         n = length(curve)
         pts = Array{Float64}(undef, 2, n)
-        for (i,pt) in enumerate(curve)
-            pts[1,i] = pt.pos.x
-            pts[2,i] = pt.pos.y
+        for (i, pt) in enumerate(curve)
+            pts[1, i] = pt.pos.x
+            pts[2, i] = pt.pos.y
         end
-        add_instruction!(rendermodel, render_line, (pts, colorant"grey", sw.width, Cairo.CAIRO_LINE_CAP_BUTT))
+        add_instruction!(
+            rendermodel,
+            render_line,
+            (pts, colorant"grey", sw.width, Cairo.CAIRO_LINE_CAP_BUTT)
+        )
     end
 
     # Render roadway
@@ -120,14 +139,18 @@ function AutomotiveVisualization.add_renderable!(rendermodel::RenderModel, env::
     curve = env.crosswalk.curve
     n = length(curve)
     pts = Array{Float64}(undef, 2, n)
-    for (i,pt) in enumerate(curve)
-        pts[1,i] = pt.pos.x
-        pts[2,i] = pt.pos.y
+    for (i, pt) in enumerate(curve)
+        pts[1, i] = pt.pos.x
+        pts[2, i] = pt.pos.y
     end
 
     # We can add render instructions from AutomotiveVisualization.
     # Here we want the crosswalk to appear as a white-striped zebra crossing rather than a road.
-    add_instruction!(rendermodel, render_dashed_line, (pts, colorant"white", env.crosswalk.width, 1.0, 1.0, 0.0, Cairo.CAIRO_LINE_CAP_BUTT))
+    add_instruction!(
+        rendermodel,
+        render_dashed_line,
+        (pts, colorant"white", env.crosswalk.width, 1.0, 1.0, 0.0, Cairo.CAIRO_LINE_CAP_BUTT)
+    )
 
     return rendermodel
 end
@@ -156,14 +179,22 @@ timestep = 0.1
 # Crossing pedestrian definition
 
 # %%
-ped_init_state = VehicleState(VecSE2(49.0,-3.0,0.), env.sidewalk[BOTTOM], roadway, 1.3)
+ped_init_state = VehicleState(
+    VecSE2(49.0, -3.0, 0.0),
+    env.sidewalk[BOTTOM],
+    roadway,
+    1.3)
 ped = Entity(ped_init_state, PEDESTRIAN_DEF, :pedestrian)
 
 # %% [markdown]
 # Car definition
 
 # %%
-car_initial_state = VehicleState(VecSE2(0.0, 0., 0.), roadway.segments[1].lanes[1],roadway, 8.0)
+car_initial_state = VehicleState(
+    VecSE2(0.0, 0.0, 0.0),
+    roadway.segments[1].lanes[1],
+    roadway,
+    8.0)
 car = Entity(car_initial_state, VehicleDef(), :car)
 
 scene = Scene([ped, car])
@@ -172,12 +203,14 @@ scene = Scene([ped, car])
 # Define a model for each entity present in the scene
 
 # %%
-models = Dict{Symbol, DriverModel}(
+models = Dict{Symbol,DriverModel}(
     :pedestrian => SidewalkPedestrianModel(
-        timestep=timestep, crosswalk=env.crosswalk,
-        sw_origin = env.sidewalk[BOTTOM], sw_dest = env.sidewalk[TOP]
+        timestep=timestep,
+        crosswalk=env.crosswalk,
+        sw_origin=env.sidewalk[BOTTOM],
+        sw_dest=env.sidewalk[TOP]
     ),
-    :car        => LatLonSeparableDriver( # produces LatLonAccels
+    :car => LatLonSeparableDriver( # produces LatLonAccels
         ProportionalLaneTracker(), # lateral model
         IntelligentDriverModel(), # longitudinal model
     )
@@ -195,8 +228,8 @@ Reel.set_output_type("gif") # or "mp4" https://github.com/shashi/Reel.jl#in-ijul
 nticks = 300
 scenes = simulate(scene, roadway, models, nticks, timestep)
 
-animation = roll(fps=1.0/timestep, duration=nticks*timestep) do t, dt
-    i = Int(floor(t/dt)) + 1
+animation = roll(fps=1.0 / timestep, duration=nticks * timestep) do t, dt
+    i = Int(floor(t / dt)) + 1
     render([env, scenes[i]])
 end;
 
